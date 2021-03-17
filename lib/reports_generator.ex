376707c16040d462c1d475dd84de755082082058
Ana Paula Souza <ana.pdsouza@b2wdigital.com>
2021-03-16 22:24:19 -0300

    Lendo arquivo em um stream

---
+++ lib/reports_generator.ex
@@ -0,0 +1,18 @@
+defmodule ReportsGenerator do
+  @moduledoc """
+  Projeto 1
+  """
+
+  def build(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Enum.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
+end
