3cf57f4b4f445f79a5150163af35f82cda9c7152
Ana Paula Souza <ana.pdsouza@b2wdigital.com>
2021-04-16 20:20:49 -0300

    Gerando relatorio

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,18 +1,48 @@
 defmodule ReportsGenerator do
   @moduledoc """
-  Projeto 1
+    Projeto 1
+      Módulo para parsear os valores.
   """
 
+  alias ReportsGenerator.Parser
+
+  #Variavel de modulo - funciona dentro do modulo que tiver o @available_foods
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @options ["foods", "users"]
+
   def build(filename) do
-    "reports/#{filename}"
-    |> File.stream!()
-    |> Enum.map(fn line -> parse_line(line) end)
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
-  defp parse_line(line) do
-    line
-    |> String.trim()
-    |> String.split(",")
-    |> List.update_at(2, &String.to_integer/1)
+  def fetch_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
+
+  def fetch_higher_cost(_report, _option), do: {:erros, "Invalid option!"}
+
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  defp report_acc do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    %{"users" => users, "foods" => foods}
   end
 end
